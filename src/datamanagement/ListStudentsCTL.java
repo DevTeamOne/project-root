1b581a1d4b9ef552279ce986d4613d1710a356d9
Evan Watkins <evwatkins@gmail.com>
2015-07-28 20:39:55 +1000

    Reformatted for readability

--- src/datamanagement/ListStudentsCTL.java
+++ src/datamanagement/ListStudentsCTL.java
@@ -1,8 +1,16 @@
 package datamanagement;
-        public class ListStudentsCTL {
-private StudentManager sm;
-public ListStudentsCTL() {sm = StudentManager.get();}
-            public void listStudents( IStudentLister lister, String unitCode ) {
+
+public class ListStudentsCTL {
+  private StudentManager sm;
+
+  public ListStudentsCTL() {
+    sm = StudentManager.get();
+  }
+
+  public void listStudents(IStudentLister lister, String unitCode) {
     lister.clearStudents();
-                StudentMap students = sm.getStudentsByUnit( unitCode );
-for (Integer id : students.keySet() ) lister.addStudent(students.get(id));}}
+    StudentMap students = sm.getStudentsByUnit(unitCode);
+    for (Integer id : students.keySet())
+      lister.addStudent(students.get(id));
+  }
+}
