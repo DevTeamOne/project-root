5469cfddcb9736d0ae3d5a8aef5916b73878eb0f
piriej <jpirie@iinet.net.au>
2015-07-31 17:02:17 +1000

    Modified spacing, removed * imports, cleaned up variable names.

--- src/datamanagement/AppProperties.java
+++ src/datamanagement/AppProperties.java
@@ -1,29 +1,30 @@
 package datamanagement;
 
-import java.util.*;
-import java.io.*;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class AppProperties {
-	private static AppProperties self = null;
-	private Properties properties;
+  private static AppProperties self = new AppProperties();
+  private Properties properties;
 
-	public static AppProperties getInstance() {
-		if (self == null) {
-			self = new AppProperties();
-		}
-		return self;
-	}
+  
+  public static AppProperties getInstance() {
+    return self;
+  }
 
-	private AppProperties() {
-		properties = new Properties();
-		try {
-			properties.load(new FileInputStream("Properties.prop"));
-		} catch (IOException e) {
-			throw new RuntimeException("Could not read property file");
-		}
-	}
+  
+  private AppProperties() {
+    properties = new Properties();
+    try {
+      properties.load(new FileInputStream("Properties.prop"));
+    } catch (IOException exception) {
+      throw new RuntimeException("Could not read property file");
+    }
+  }
 
-	public Properties getProperties() {
-		return properties;
-	}
+  
+  public Properties getProperties() {
+    return properties;
+  }
 }
