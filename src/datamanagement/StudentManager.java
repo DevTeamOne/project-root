e2d75353979da6588f3176dbe62149661a382333
Evan Watkins <evwatkins@gmail.com>
2015-08-05 23:38:16 +1000

    Continued to modify StudentManager.java

--- src/datamanagement/StudentManager.java
+++ src/datamanagement/StudentManager.java
@@ -24,17 +24,17 @@ public class StudentManager {
   }
 
   public IStudent getStudent(Integer id) {
-    IStudent is = studentManager.get(id);
+    IStudent individualStudent = studentManager.get(id);
     
-    return is != null ? is : createStudent(id);
+    return individualStudent != null ? individualStudent : createStudent(id);
   }
   
   private Element getStudentElement(Integer id) {
-    for (Element el : (List<Element>) XMLManager.getXML().getDocument()
+    for (Element element : (List<Element>) XMLManager.getXML().getDocument()
         .getRootElement().getChild("studentTable").getChildren("student"))
       
-      if (id.toString().equals(el.getAttribute("sid"))) {
-        return el;
+      if (id.toString().equals(element.getAttribute("Student Number"))) {
+        return element;
       }
     return null;
   }
@@ -48,32 +48,33 @@ public class StudentManager {
 
     student = new StudentMap();
     
-    IStudent is;
+    IStudent individualStudent;
     
     StudentUnitRecordList unitRecord = StudentUnitRecordManager.instance().getRecordsByUnit(unitCode);
     
     for (IStudentUnitRecord S : unitRecord) {
-      is = createStudentProxy(new Integer(S.getStudentID()));
-      student.put(is.getStudentNumber(), is);
+      individualStudent = createStudentProxy(new Integer(S.getStudentID()));
+      student.put(individualStudent.getStudentNumber(), individualStudent);
     }
 
     unitManager.put(unitCode, student);
+    
     return student;
   }
 
   private IStudent createStudent(Integer studentNumber) {
-    IStudent is;
+    IStudent individualStudent;
     Element element = getStudentElement(studentNumber);
     
     if (element != null) {
       StudentUnitRecordList rlist = StudentUnitRecordManager.instance().getRecordsByStudent(studentNumber);
       
-      is = new Student(new Integer(element.getAttribute("sid")),
-          element.getAttribute("fname"), element.getAttribute("lname"), rlist);
+      individualStudent = new Student(new Integer(element.getAttribute("Student Number")),
+          element.getAttribute("First Name"), element.getAttribute("Last Name"), rlist);
 
-      studentManager.put(is.getStudentNumber(), is);
+      studentManager.put(individualStudent.getStudentNumber(), individualStudent);
       
-      return is;
+      return individualStudent;
     }
     
     throw new RuntimeException("DBMD: createStudent : student not in file");
@@ -83,7 +84,7 @@ public class StudentManager {
     Element element = getStudentElement(studentNumber);
 
     if (element != null) {
-      return new StudentProxy(studentNumber, element.getAttribute("fname"), element.getAttribute("lname"));
+      return new StudentProxy(studentNumber, element.getAttribute("First Name"), element.getAttribute("Last Name"));
     }
     throw new RuntimeException("DBMD: createStudent : student not in file");
   }
