563aa2a3c8d0ec1cfd7d9ec0a3f1ccb15e149ea4
Evan Watkins <evwatkins@gmail.com>
2015-08-16 23:09:29 +1000

    Comments updated on ListStudentsControl.java

--- src/datamanagement/ListStudentsControl.java
+++ src/datamanagement/ListStudentsControl.java
@@ -3,21 +3,29 @@
  * Student Number: 11537439
  * Class: ITC515
  * Assessment: Assignment 2
- * Description: This class 
+ * Description: This class controls a list of students by student code.
  */
 package datamanagement;
 
 public class ListStudentsControl {
+  
+  /** 
+   * Declare class variables.
+   */
   private StudentManager studentManager;
 
-  
-  
+  /**
+   * Constructor of class ListStudentsControl.
+   */ 
   public ListStudentsControl() {
     studentManager = StudentManager.get();
   }
 
-  
-  
+  /**
+   * Method call from studentManager.java and student.java.
+   * @param lister: The list of students retrieved.
+   * @param code: locate student by looking up unit code.
+   */  
   public void listStudents(IStudentLister lister, String code) {
     lister.clearStudents();
     
