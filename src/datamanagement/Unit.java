63b81eab25d4df5e481067df7c12f0aa8667bdc8
Evan Watkins <evwatkins@gmail.com>
2015-08-17 23:35:56 +1000

    Final changes made to Unit.java

--- src/datamanagement/Unit.java
+++ src/datamanagement/Unit.java
@@ -24,29 +24,29 @@ public class Unit implements UnitInterface {
 
   /**
    * Retrieve a unit or create new unit if no existing match.
-   * @param code: The unit code to retrieve
-   * @param name: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param unitCode: The unit code to retrieve
-   * @param recordList: The unit code to retrieve     
+   * @param code: The unit code to retrieve.
+   * @param name: The unit name to retrieve.
+   * @param range1: The range of a pass mark for this unit to retrieve.
+   * @param range2: The range of a credit mark for this unit to retrieve.
+   * @param range3: The range of a distinction mark for this unit to retrieve.
+   * @param range4: The range of a high distinction mark for this unit to retrieve.
+   * @param range5: The range of an additional exam for this unit to retrieve.
+   * @param weight1: The weight of assessments for this unit to set.
+   * @param weight2: The weight of assessments for this unit to set.
+   * @param weight3: The weight of assessments for this unit to set.
+   * @param recordList: If the unit is not currently in the unit list then create new unit to record. 
    */
-  public Unit(String code, String name, float f1, float f2, float f3, float f4,
-      float f5, int i1, int i2, int i3, StudentUnitRecordList recordList) {
+  public Unit(String code, String name, float range1, float range2, float range3, float range4,
+      float range5, int weight1, int weight2, int weight3, StudentUnitRecordList recordList) {
 
     this.unitCode_ = code;
     this.unitName_ = name;
-    this.passRange = f1;
-    this.creditRange = f2;
-    this.distinctionRange = f3;
-    this.highDistinctionRange = f4;
-    this.additionalExamRange = f5;
-    this.setAssessmentWeights(i1, i2, i3);
+    this.passRange = range1;
+    this.creditRange = range2;
+    this.distinctionRange = range3;
+    this.highDistinctionRange = range4;
+    this.additionalExamRange = range5;
+    this.setAssessmentWeights(weight1, weight2, weight3);
     recordStudent = recordList == null ? new StudentUnitRecordList() : recordList;
   }
 
@@ -146,11 +146,10 @@ public class Unit implements UnitInterface {
   }
   
   /**
-   * Retrieve high distinction range.
-   * @param studentNumber: The student number to retrieve.
-   * @param
-   * @param
-   * @param
+   * Retrieve grades.
+   * @param assessment1: The first assessment value to retrieve.
+   * @param assessment2: The second assessment value to retrieve.
+   * @param assessment3: The third assessment value to retrieve.
    * @throw exception on marking criterion.
    * @return string.
    * @return string.
@@ -159,23 +158,26 @@ public class Unit implements UnitInterface {
    * @return string.
    * @return string.
    */
-  public String getGrade(float f1, float f2, float f3) {
-    float t = f1 + f2 + f3;
+  public String getGrade(float assessment1, float assessment2, float assessment3) {
+    float totalMarks = assessment1 + assessment2 + assessment3;
 
-    if (f1 < 0 || f1 > assignment1 || f2 < 0 || f2 > assignment2 || f3 < 0 || f3 > exam) {
+    if (assessment1 < 0 || assessment1 > assignment1 || 
+        assessment2 < 0 || assessment2 > assignment2 || 
+        assessment3 < 0 || assessment3 > exam) {
       throw new RuntimeException(
           "marks cannot be less than zero or greater than assessment weights");
     }
 
-    if (t < additionalExamRange) {
+    if (totalMarks < additionalExamRange) {
       return "Fail";
-    } else if (t < passRange)
+    } 
+    else if (totalMarks < passRange)
       return "Additional Exam";
-    else if (t < creditRange)
+    else if (totalMarks < creditRange)
       return "Pass";
-    else if (t < distinctionRange)
+    else if (totalMarks < distinctionRange)
       return "Credit";
-    else if (t < highDistinctionRange)
+    else if (totalMarks < highDistinctionRange)
       return "Distinction";
     else
       return "High Distinction";
@@ -226,18 +228,22 @@ public class Unit implements UnitInterface {
    * @param assessment1: The weight of assessment 1 to set.
    * @param assessment2: The weight of assessment 2 to set.
    * @param assessmentExam: The weight of exam to set.
+   * @throw exception on weight range.
+   * @throw exception on weight total.
    */
-  public void setAssessmentWeights(int assessment1, int assessment2, int assessmentExam) {
-    if (assessment1 < 0 || assessment1 > 100 || assessment2 < 0 || assessment2 > 100 || assessmentExam < 0 || assessmentExam > 100) {
+  public void setAssessmentWeights(int assessment1, int assessment2, int assessment3) {
+    if (assessment1 < 0 || assessment1 > 100 || 
+        assessment2 < 0 || assessment2 > 100 || 
+        assessment3 < 0 || assessment3 > 100) {
       throw new RuntimeException(
           "Assessment weights cant be less than zero or greater than 100");
     }
-    if (assessment1 + assessment2 + assessmentExam != 100) {
+    if (assessment1 + assessment2 + assessment3 != 100) {
       throw new RuntimeException("Assessment weights must add to 100");
     }
     this.assignment1 = assessment1;
     this.assignment2 = assessment2;
-    this.exam = assessmentExam;
+    this.exam = assessment3;
   }
 
   /**
@@ -249,8 +255,11 @@ public class Unit implements UnitInterface {
    * @param additionalExam: The additional exam range to set.
    */
   private void setAssignmentRange(float pass, float credit, float distinction, float highDistinction, float additionalExam) {
-    if (pass < 0 || pass > 100 || credit < 0 || credit > 100 || distinction < 0 || distinction > 100
-        || highDistinction <= 0 || highDistinction > 100 || additionalExam < 0 || additionalExam > 100) {
+    if (pass < 0 || pass > 100 || 
+        credit < 0 || credit > 100 || 
+        distinction < 0 || distinction > 100 || 
+        highDistinction <= 0 || highDistinction > 100 || 
+        additionalExam < 0 || additionalExam > 100) {
       throw new RuntimeException(
           "Assessment range cant be less than zero or greater than 100");
     }
