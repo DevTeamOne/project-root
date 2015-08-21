5e6353ce56520ce84fe461a821106a1455daf5eb
piriej <jpirie@iinet.net.au>
2015-08-21 13:38:38 +1000

    Updated review 13 changes

--- src/datamanagement/ApplicationProperties.java
+++ src/datamanagement/ApplicationProperties.java
@@ -5,30 +5,45 @@ import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+/**
+ * Concrete class for the application properties.
+ */
 public class ApplicationProperties {
   private static ApplicationProperties self_ = new ApplicationProperties();
-  private Properties properties_;
+  private Properties properties;
   
   
-  
+
   private ApplicationProperties() {
-    properties_ = new Properties();
+    properties = new Properties();
     try {
-      properties_.load(new FileInputStream("Properties.prop"));
+      properties.load(new FileInputStream("Properties.prop"));
     } catch (IOException ioException) {
       throw new RuntimeException("Could not read property file");
     }
   }
   
   
-  
+
+  /**
+   * Returns the single instance of the application
+   * properties object.
+   * 
+   * @return The instance of the application properties
+   * object.
+   */
   public static ApplicationProperties getInstance() {
     return self_;
   }
 
   
   
+  /**
+   * The application properties collection.
+   * 
+   * @return the application properties collection.
+   */
   public Properties getProperties() {
-    return properties_;
+    return properties;
   }
 }
